891ca752d2676358d5ec60416905900d39262f44
Xavier P <44008221+Xavosorcerer@users.noreply.github.com>
2019-02-22 08:52:05 -0500

    I did it!
    I understand my new code and fixed it. cool

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ var bSize = 56;
 var bLeng = bSize * 1.5;
 var xPos = [];
 var reps = 20; //horizontal repititions
-var leng = ceil(bSize / 400); // vertical repititions
+var leng = ceil(400 / bSize); // vertical repititions
 var start = -bLeng +10;
 var c1 = new Array(reps);
 var c2 = new Array(reps);
@@ -19,7 +19,7 @@ for (var d = 0; d < reps; d++) {
     var t = d * bSize;
     xPos.push(t);
     //color for each shape
-    for (var c = 0; c <= leng + 5; c++) {
+    for (var c = 0; c <= leng; c++) {
         c1[d][c] = random(0, 255);
         c2[d][c] = random(0, 255);
         c3[d][c] = random(0, 255);
@@ -32,7 +32,7 @@ draw = function() {
     //for the lines
     for (var i = reps; i >= 0; i-= 1) {
         //for the individual blocks
-        for (var l = 0; l <= (400 / bSize) + 1; l++) {
+        for (var l = 0; l <= (leng) + 1; l++) {
             var x = xPos[i];
             var y = l * bSize;
             fill(c1[i][l], c2[i][l], c3[i][l]);
@@ -40,8 +40,8 @@ draw = function() {
             rect(x, y, bSize, bSize);
             triangle(x + bSize, y, x + bSize * 1.5, y + bSize * 0.5, x + bSize, y + bSize);
             triangle(x, y, x + bSize * 1.54, y - bSize * 0.5, x + bSize, y);
-            xPos[i]+= 0.5;
-            if (xPos[i] >= 1102/*bSize * (reps + 1) + start*/) {
+            xPos[i]+= 0.1;
+            if (xPos[i] >= /*1102*/bSize * (reps + 1) + start) {
                 xPos[i] = start;
             }
         }
@@ -51,6 +51,6 @@ draw = function() {
     rect(200, 200, 20, 20);*/
     
 };
-for (var i = 0; i <=c1.length; i++) {
+/*for (var i = 0; i <=c1.length; i++) {
 println(c1[i]);
-}
+}*/

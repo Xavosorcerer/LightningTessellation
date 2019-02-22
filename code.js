101e48e720da836facc7c8354384b33286f12855
Xavier P <44008221+Xavosorcerer@users.noreply.github.com>
2019-02-22 08:42:24 -0500

    working but weirdly
    I want to find out why my code doesn't work as I think it should.

--- code.js
+++ code.js
@@ -1,20 +1,28 @@
 var bSize = 56;
 var bLeng = bSize * 1.5;
 var xPos = [];
-var c1 = [[], [], [],];
-var c2 = [];
-var c3 = [];
-var reps = 20;
+var reps = 20; //horizontal repititions
+var leng = ceil(bSize / 400); // vertical repititions
 var start = -bLeng +10;
+var c1 = new Array(reps);
+var c2 = new Array(reps);
+var c3 = new Array(reps);
 
-//each individual line
+for (var t = 0; t < reps + 1; t++) {
+    c1[t] = new Array(leng);
+    c2[t] = new Array(leng);
+    c3[t] = new Array(leng);
+}
+
+//color and position for each individual line
 for (var d = 0; d < reps; d++) {
     var t = d * bSize;
     xPos.push(t);
-    for (var c = 0; c <= 1 +(bSize / 400); c++) {
-        c1.push(random(0,255));
-        c2.push(random(0,255));
-        c3.push(random(0,255));
+    //color for each shape
+    for (var c = 0; c <= leng + 5; c++) {
+        c1[d][c] = random(0, 255);
+        c2[d][c] = random(0, 255);
+        c3[d][c] = random(0, 255);
     }
 }
 
@@ -27,7 +35,7 @@ draw = function() {
         for (var l = 0; l <= (400 / bSize) + 1; l++) {
             var x = xPos[i];
             var y = l * bSize;
-            fill(c1[i], c2[i], c3[i]);
+            fill(c1[i][l], c2[i][l], c3[i][l]);
             noStroke();
             rect(x, y, bSize, bSize);
             triangle(x + bSize, y, x + bSize * 1.5, y + bSize * 0.5, x + bSize, y + bSize);
@@ -43,3 +51,6 @@ draw = function() {
     rect(200, 200, 20, 20);*/
     
 };
+for (var i = 0; i <=c1.length; i++) {
+println(c1[i]);
+}
